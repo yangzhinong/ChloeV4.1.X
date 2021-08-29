26fcbc8e9867fed0b0775caabbf3861963ccd7a5
yzn <66845701@qq.com>
2021-08-29 17:02:37 +0800

    同频 主版V4.6.0 Fixes bug: q.IncludeMany(a=>a.Cityes).thenIncludeMany(a=>a.Persons).ThenInclude(a=>a.Ex).ToList();

--- src/Chloe/Query/QueryExpressions/IncludeExpression.cs
+++ src/Chloe/Query/QueryExpressions/IncludeExpression.cs
@@ -5,13 +5,14 @@ using System.Reflection;
 
 namespace Chloe.Query.QueryExpressions
 {
-    class IncludeExpression : QueryExpression
+    internal class IncludeExpression : QueryExpression
     {
         public IncludeExpression(Type elementType, QueryExpression prevExpression, NavigationNode navigationNode)
            : base(QueryExpressionType.Include, elementType, prevExpression)
         {
             this.NavigationNode = navigationNode;
         }
+
         public NavigationNode NavigationNode { get; private set; }
 
         public override T Accept<T>(QueryExpressionVisitor<T> visitor)
@@ -25,10 +26,12 @@ namespace Chloe.Query.QueryExpressions
         public NavigationNode()
         {
         }
+
         public NavigationNode(PropertyInfo property)
         {
             this.Property = property;
         }
+
         public PropertyInfo Property { get; set; }
         public LambdaExpression Condition { get; set; }
         public List<LambdaExpression> ContextFilters { get; private set; } = new List<LambdaExpression>();
@@ -40,7 +43,7 @@ namespace Chloe.Query.QueryExpressions
             current.ContextFilters.AddRange(this.ContextFilters);
             if (this.Next != null)
             {
-                current.Next = current.Clone();
+                current.Next = this.Next.Clone();
             }
 
             return current;
@@ -52,6 +55,14 @@ namespace Chloe.Query.QueryExpressions
                 return this;
 
             return this.Next.GetLast();
+        }
+
+        public override string ToString()
+        {
+            if (this.Next == null)
+                return this.Property.Name;
+
+            return $"{this.Property.Name}.{this.Next.ToString()}";
         }
     }
 }
