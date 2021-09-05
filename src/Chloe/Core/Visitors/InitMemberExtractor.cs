172bbaecd35d98e4a0f44e42c7b37e2fb926de99
yzn <66845701@qq.com>
2021-09-05 22:50:24 +0800

    Lambda更新语法增强 var test= new Test() { Pat = "y'zn", Visit = u.Id, Val = 44 }; context.Update( x => x.Pat == "yzn" && x.Visit == 1, x => test); //这里不再需要写成 new Test(){....}

--- src/Chloe/Core/Visitors/InitMemberExtractor.cs
+++ src/Chloe/Core/Visitors/InitMemberExtractor.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Chloe.Extensions;
+using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Reflection;
@@ -7,14 +8,17 @@ namespace Chloe.Core.Visitors
 {
     public class InitMemberExtractor : ExpressionVisitor<Dictionary<MemberInfo, Expression>>
     {
-        static readonly InitMemberExtractor _extractor = new InitMemberExtractor();
-        InitMemberExtractor()
+        private static readonly InitMemberExtractor _extractor = new InitMemberExtractor();
+
+        private InitMemberExtractor()
         {
         }
+
         public static Dictionary<MemberInfo, Expression> Extract(Expression exp)
         {
             return _extractor.Visit(exp);
         }
+
         public override Dictionary<MemberInfo, Expression> Visit(Expression exp)
         {
             if (exp == null)
@@ -23,17 +27,39 @@ namespace Chloe.Core.Visitors
             switch (exp.NodeType)
             {
                 case ExpressionType.Lambda:
-                    return this.VisitLambda((LambdaExpression)exp);
+                    {
+                        LambdaExpression lambdaExp = exp as LambdaExpression;
+                        if (lambdaExp.Body is MemberExpression)
+                        {
+                            Dictionary<MemberInfo, Expression> ret = new Dictionary<MemberInfo, Expression>();
+                            var body = lambdaExp.Body as MemberExpression;
+                            var obj = lambdaExp.Compile().DynamicInvoke(new object[] { null });
+                            foreach (PropertyInfo prop in body.Type.GetProperties())
+                            {
+                                var v = prop.GetValue(obj, null);
+                                ret.Add(prop, ExpressionExtension.MakeWrapperAccess(v, prop.PropertyType));
+                            }
+                            return ret;
+                        }
+                        else
+                        {
+                            return this.VisitLambda((LambdaExpression)exp);
+                        }
+                    }
+
                 case ExpressionType.MemberInit:
                     return this.VisitMemberInit((MemberInitExpression)exp);
+
                 default:
                     throw new Exception(string.Format("Unhandled expression type: '{0}'", exp.NodeType));
             }
         }
+
         protected override Dictionary<MemberInfo, Expression> VisitLambda(LambdaExpression exp)
         {
             return this.Visit(exp.Body);
         }
+
         protected override Dictionary<MemberInfo, Expression> VisitMemberInit(MemberInitExpression exp)
         {
             Dictionary<MemberInfo, Expression> ret = new Dictionary<MemberInfo, Expression>(exp.Bindings.Count);
